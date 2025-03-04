cfa1dd19738dd8cc9b09687ed09841e7c2041f2f
Buffy-Official <buffy@gamingsmp.net>
2025-03-04 19:21:38 +0800

    Added BreakBlock.java

---
+++ src/main/java/GuardX/BreakBlock.java
@@ -0,0 +1,13 @@
+package GuardX;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public final class BreakBlock implements Listener {
+
+    @EventHandler
+    public void onBreakBlock(BlockBreakEvent event) {
+// No code yet, will be added soon.
+    }
+}
