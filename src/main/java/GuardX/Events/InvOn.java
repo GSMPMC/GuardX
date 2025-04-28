8511e10d811ef65f9a1b4b2d33a54327ffb02a62
Buffy-Official <buffy@gamingsmp.net>
2025-04-28 10:23:12 +0800

    Added KeepInv to on even if the Gamerule is set to false. Will add a config file soon.

--- src/main/java/GuardX/Events/InvOn.java
+++ src/main/java/GuardX/Events/InvOn.java
@@ -1,7 +1,6 @@
 package GuardX.Events;
 
 import org.bukkit.ChatColor;
-import org.bukkit.damage.DeathMessageType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -10,7 +9,6 @@ public class InvOn implements Listener {
 
     public void onPDeath(PlayerDeathEvent e){
         Player p = e.getPlayer();
-
         e.setKeepLevel(true);
         e.setKeepInventory(true);
         e.setDeathMessage(ChatColor.YELLOW + p.getName() + "Access Denied to their Life Support!");
